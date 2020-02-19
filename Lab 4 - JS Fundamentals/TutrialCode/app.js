e2e373890c528920643cae59aa69dc142190c8b0
abdalabaaji <abdala.baaji@gmail.com>
2020-02-19 20:38:35 +0300

    update

--- Lab 4 - JS Fundamentals/TutrialCode/app.js
+++ Lab 4 - JS Fundamentals/TutrialCode/app.js
@@ -136,3 +136,99 @@ let y=11;
 // isEven(-123);
 
 //Arrays
+//
+// console.log('Hello');
+// console.error("Error message");
+// console.warn("Warning");
+
+
+// let numbers = [2,4,18,28,9,5,6,7,8,9];
+
+// numbers.push(55555);
+// console.log(numbers);
+//
+// numbers.pop();
+// console.log(numbers);
+//
+// numbers.shift();
+// console.log(numbers);
+//
+// numbers.unshift(266)
+// console.log(numbers);
+
+let numbers = [2,4,18,28,19,15,16,7,8,9];
+// let names = ["Abdulahi" , "Ali" , "Sara", "Mohamed"];
+// // names.sort();
+// console.log(names);
+//
+// console.log('Reverse Strings');
+// console.log(names.reverse());
+//
+// let compareFun = (x, y) =>x-y;
+//
+// console.log('Increasing');
+// numbers.sort(compareFun);
+// console.log(numbers);
+//
+// //
+// //
+// console.log('Decreasing');
+// numbers.reverse();
+// console.log(numbers);
+//
+
+
+
+// function decreasingOrder(x, y) {
+//     return y-x;
+// }
+
+//Spread operator
+// let numbers2 = [2,4,18,28,19,15,16,7,8,9, ];
+// let numbers3 = [-1,-2,-3, [1,2,3 , [4,4,4] ]];
+//
+// console.log(numbers3[3][0]);
+//
+// let combinedArray = [...numbers2 , ...numbers3];
+// console.log(combinedArray);
+//
+// combinedArray = [...numbers2 , ...numbers3.flat(2)];
+// console.log(combinedArray);
+
+
+// //finding something inside an array
+// let numbers5 = [1,2,3,4];
+//
+// console.log("The maximum value " + Math.max(...numbers5));
+//
+// console.log(numbers5);
+// console.log(...numbers5);
+//
+// let oneOfTheNumbe = numbers5.find(x=> x==-15)
+// let negativeNumbersInsideTheArray = numbers5.filter(x=> x<0);
+//
+// let combines =  (x, y) => x+y;
+//
+// let sum = numbers5.reduce((prev , curr) => curr + prev);
+// let mul = numbers5.reduce((prev , curr) => curr * prev);
+// let biggest = numbers5.reduce((prev , curr) => curr > prev ? curr : prev);
+// let minimum = numbers5.reduce((prev , curr) => curr > prev ? prev : curr);
+//
+// console.log(`Sum = ${sum}`);
+// console.log(`Mult = ${mul}`);
+// console.log(`Biggest = ${biggest}`);
+// console.log(`Min = ${minimum}`);
+
+numbers = [1,2,3,4,5,6]
+for (let i = 0; i < numbers.length; i++) {
+    i = i*i;
+}
+//
+// function display (v){
+//     console.log(v**2);
+// }
+
+
+console.log(numbers.forEach( x=> x**2))
+
+// numbers.forEach( display)
