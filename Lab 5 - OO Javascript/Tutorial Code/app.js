bbd9bf03d031ba61c8e26a553d8bfde714a35265
abdalabaaji <abdala.baaji@gmail.com>
2020-02-26 16:37:05 +0300

    update 4:37

---
+++ Lab 5 - OO Javascript/Tutorial Code/app.js
@@ -0,0 +1,86 @@
+// //object literals
+//
+// function CourseF(name , code){
+//     this.name = name;
+//     this.code = code;
+// }
+//
+// class CourseC{
+//     constructor(name, code) {
+//         this.name = name;
+//         this.code = code;
+//     }
+// }
+//
+// let cCourse = new CourseC("Data Structures" , "CMPS303")
+// let fCourse = new CourseF("Data Structures" , "CMPS303");
+//
+//
+//
+// let course1 =  {
+//     name : "Enterprise App Dev.",
+//     code : "CMPS-356",
+//     display : function display() {
+//         console.log(`Name ${this.name} Code : ${this.code}`);
+//     }
+// }
+// var student = "ali";
+// let course2 =  {
+//     name : "Object Oriented Prorgamming",
+//     code : "CMPS-251",
+//     display() {
+//         console.log(`Name ${this.name} Code : ${this.code}`);
+//     }
+// }
+//
+// course2.someNewPropery = "Woow";
+//
+// console.log(course2.someNewPropery);
+// course1.display();
+// course2.display();
+//
+//
+//
+
+class Course {
+    constructor(name, code) {
+        this.name = name;
+        this.code = code;
+    }
+    display() {
+        console.log(`Name ${this.name} Code : ${this.code}`);
+    }
+}
+
+const course = new Course("Data Structures", "CMPS-303");
+const course2 = new Course("OOM", "CMPS251");
+
+course.display();
+course2.display();
+
+class Student {
+    constructor(id, name , age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    xyz() {
+        console.log(`Name :${name} `);
+    }
+}
+
+
+const student = new Student(123, "Abdulah", 100);
+student.xyz();
+
+
+
+
+
+
+
+
+
+
+
