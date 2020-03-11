1efcbac496b2c459fc5c063a604eb88ad7fc44f3
abdalabaaji <abdala.baaji@gmail.com>
2020-03-11 16:35:42 +0300

    update 4:25

--- Lab 6 - Async JS/UnitConverter/test/UnitConverter.spec.js
+++ Lab 6 - Async JS/UnitConverter/test/UnitConverter.spec.js
@@ -1,6 +1,14 @@
 const unitConverter = require('../UnitConverter');
-const expect = require('chai')
+const expect = require('chai').expect;
 
-describe('Test cases for the UnitConverter Class', function () {
+describe('Test cases for the UnitConverter Class',  ()=> {
+    it('1 kgToOunce should be 35.274',  ()=> {
+        const val= unitConverter.kgToOunce(1); //35.274
+        expect(val).to.equal(35.274)
+    });
+    it('1 kgToPound Should be 4.4092', ()=>{
+        expect(unitConverter.kgToPound(1)).to.equal(2.2046)
+    });
+
     
 });
