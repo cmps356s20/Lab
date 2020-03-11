3abb665f944f09f2910e2cae12445a053c14642e
abdalabaaji <abdala.baaji@gmail.com>
2020-03-11 16:50:05 +0300

    Update UnitConverter.spec.js

--- Lab 6 - Async JS/UnitConverter/test/UnitConverter.spec.js
+++ Lab 6 - Async JS/UnitConverter/test/UnitConverter.spec.js
@@ -1,14 +1,33 @@
-const unitConverter = require('../UnitConverter');
+const unitConverterSpec = require('../UnitConverter');
 const expect = require('chai').expect;
 
 describe('Test cases for the UnitConverter Class',  ()=> {
-    it('1 kgToOunce should be 35.274',  ()=> {
-        const val= unitConverter.kgToOunce(1); //35.274
-        expect(val).to.equal(35.274)
-    });
-    it('1 kgToPound Should be 4.4092', ()=>{
-        expect(unitConverter.kgToPound(1)).to.equal(2.2046)
+
+    //weight
+
+    describe('Weight testing functions',  ()=> {
+        it('1 kgToOunce should be 35.274',  ()=> {
+            const val= unitConverterSpec.kgToOunce(1); //35.274
+            if(val==35.274)
+                return true;
+            else
+                return false;
+
+             expect(val).to.equal(35.274)
+        });
+        it('1 kgToPound Should be 4.4092', ()=>{
+            expect(unitConverterSpec.kgToPound(1)).to.equal(2.2046)
+        });
     });
 
-    
+    describe('Distance/Length testing functions',  ()=> {
+        //distance
+        it('1 meterToInch  Should be 39.3701', () => {
+            expect(unitConverterSpec.meterToInch(1)).to.equal(39.3701)
+        });
+        it('1 meterToFoot   Should be 6.5617 ', () => {
+            expect(unitConverterSpec.meterToFoot(2)).to.equal(6.5617)
+        });
+    });
+
 });
