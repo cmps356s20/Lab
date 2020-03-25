9d8dc0d12bf298b2fd5e478c4bb23842f0edc856
abdalabaaji <abdala.baaji@gmail.com>
2020-03-25 23:24:49 +0300

    update

--- Lab 7-WebAPI/BankingApp/app.js
+++ Lab 7-WebAPI/BankingApp/app.js
@@ -10,35 +10,33 @@ const port = 5000;
 app.use(morgan('dev'));
 app.use(express.json()) //user will send me an object
 //body will parse the data and it put it back to  req.body()
-
-
-app.get('/api/accounts/accountType/:acctType', async (req, res)=>{
+app.get('/api/accounts/accountType/:acctType', async (req, res) => {
     const accounts = await accountRepo.getAccounts(req.params.acctType)
     res.json(accounts)
 })
 
-app.get('/api/accounts/:id', async (req, res)=>{
+app.get('/api/accounts/:id', async (req, res) => {
     const accounts = await accountRepo.getAccount(req.params.id)
     res.json(accounts)
 })
 
-app.delete('/api/accounts/:id', async (req, res)=>{
+app.delete('/api/accounts/:id', async (req, res) => {
     const account = await accountRepo.deleteAccount(req.params.id)
 
     res.send("account deleted")
 })
 
-app.post('/api/accounts', async (req, res)=>{
+app.post('/api/accounts', async (req, res) => {
     await accountRepo.addAccount(req.body)
     res.json("account created with account No " + req.body.accountNo)
 })
 
-app.put('/api/accounts/:id', async (req, res)=>{
+app.put('/api/accounts/:id', async (req, res) => {
     await accountRepo.updateAccount(req.body)
     res.json("account created with account No " + req.body.accountNo)
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`The server has started http://localhost:${port}`)
 })
 
