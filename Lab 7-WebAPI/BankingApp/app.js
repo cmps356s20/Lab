f3c4b993027b2b5d516cabd8f5a70bfcee212038
abdalabaaji <abdala.baaji@gmail.com>
2020-03-25 17:38:31 +0300

    update

--- Lab 7-WebAPI/BankingApp/app.js
+++ Lab 7-WebAPI/BankingApp/app.js
@@ -8,13 +8,31 @@ const app = express();
 const port = 5000;
 
 app.use(morgan('dev'));
+app.use(express.json()) //user will send me an object
 
-app.get('/api/accounts/:acctType', async (req, res)=>{
+
+app.get('/api/accounts/accountType/:acctType', async (req, res)=>{
     const accounts = await accountRepo.getAccounts(req.params.acctType)
     res.json(accounts)
 })
 
+app.get('/api/accounts/:id', async (req, res)=>{
+    const accounts = await accountRepo.getAccount(req.params.id)
+    res.json(accounts)
+})
 
+app.delete('/api/accounts/:id', async (req, res)=>{
+    const account = await accountRepo.deleteAccount(req.params.id)
+
+    res.json("account deleted")
+})
+
+app.post('/api/accounts/:id', async (req, res)=>{
+    const account = req.body();
+    await accountRepo.addAccount(account) //account the data.json file
+
+    res.json("account deleted")
+})
 
 app.listen(port, ()=>{
     console.log(`The server has started http://localhost:${port}`)
